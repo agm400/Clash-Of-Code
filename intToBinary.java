9463957743faa4c9db6c0d284298d62eb435c954
Drew <drew@premove.org>
2019-11-17 15:12:32 -0500

    I added intToBinary as a new file

---
+++ intToBinary.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+String x = Integer.toBinaryString(n);
+int y=0;
+for(int i=0;i<x.length();i++){
+    if(x.charAt(i)=='1'){
+        y++;
+        
+    }
+}
+        // Write an action using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+if(y==x.length()){
+        System.out.println("true");
+        
+}
+else{
+    System.out.println("false");
+}
+    }
+}
